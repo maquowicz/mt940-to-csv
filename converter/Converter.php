d7d0161ad23121531139632a779f492005585406
terencejackson8000 <svensommerfeld1982@gmail.com>
2023-04-11 22:01:42 +0200

    Added comments

--- converter/Converter.php
+++ converter/Converter.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Converter implementation
+ */
 class Converter implements IConverter
 {
     private array $rows = [];
@@ -7,6 +10,13 @@ class Converter implements IConverter
     {
     }
 
+    
+    /**
+     * Convert the input MT940 file to a list of transactions.
+     *
+     * @param  string $input The path to the input file name
+     * @return array The list of transactions
+     */
     public function convert(string $input): array
     {
         if (!file_exists($input)) {
@@ -19,12 +29,24 @@ class Converter implements IConverter
         return $this->rows;
     }
 
+    /**
+     * Extract transactions from the given file contents using regular expressions.
+     *
+     * @param string $fileContents The contents of the input file
+     * @return array The list of transactions
+     */
     private function extractTransactions(string $fileContents): array
     {
         preg_match_all('/(?<=:61:).*?(?=:[0-9]{2}[A-Z]{0,1}:)|(?<=:86:).*?(?=:[0-9]{2}[A-Z]{0,1}:)/s', $fileContents, $matches);
         return $matches[0];
     }
 
+    /**
+     * Convert a list of transactions to rows.
+     *
+     * @param array $transactions The list of transactions
+     * @return array The list of rows
+     */
     private function convertToRows(array $transactions): array
     {
         $rows = [];
@@ -37,7 +59,14 @@ class Converter implements IConverter
         return $rows;
     }
 
-    private function convertToRow(string $transaction, string $description): Transaction
+    /**
+     * Convert a transaction to a row.
+     *
+     * @param string $transaction The transaction to convert
+     * @param string $description The description of the transaction
+     * @return Transaction|null The converted row or null if an error occurred
+     */
+    private function convertToRow(string $transaction, string $description): ?Transaction
     {
         preg_match('/(\d{6})(\d{4})?([A-Z])([A-Z]{1,2})?(\d+,\d+)?/', $transaction, $matches);
         if (sizeof($matches) !== 6) {
@@ -55,15 +84,25 @@ class Converter implements IConverter
         }
 
         $iban = null;
-        $cleanedDescription = preg_replace('/\?[0-9]{2}/', '', $description);
-        $cleanedDescription = preg_replace('/TAN: (\d{6})/', 'TAN: xxxxxx', $cleanedDescription);
-        $cleanedDescription = preg_replace('/\R+/', ' ', $cleanedDescription);
-
+        $cleanedDescription = $this->cleanDescription($description)
         preg_match('/IBAN: ([A-Z]{2}\d{2}[A-Z0-9]{14})/', $cleanedDescription, $ibanMatches);
         if (sizeof($ibanMatches) === 2) {
             $iban = $ibanMatches[1];
         }
 
         return new Transaction($date, $transactionAmount, $iban, rtrim($cleanedDescription), $type);
+    }
+
+    /**
+     * Cleans the given description by removing any special characters and replacing the TAN number with "xxxxxx".
+     * 
+     * @param string $description The description to be cleaned
+     * @return string The cleaned description
+    */
+    private function cleanDescription($description){
+        $cleanedDescription = preg_replace('/\?[0-9]{2}/', '', $description);
+        $cleanedDescription = preg_replace('/TAN: (\d{6})/', 'TAN: xxxxxx', $cleanedDescription);
+        $cleanedDescription = preg_replace('/\R+/', ' ', $cleanedDescription);
+        return $cleanedDescription;
     }
 }
--- converter/ConverterFactory.php
+++ converter/ConverterFactory.php
@@ -1,6 +1,14 @@
 <?php
+/**
+* Factory class for creating instances of the Converter class.
+*/
 class ConverterFactory{
 
+    /**
+    * Returns an instance of the Converter class
+    * 
+    * @return IConverter An instance of the Converter class.
+    */
     public function getConverter() : IConverter{
         return new Converter();
     }
